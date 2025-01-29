9eab58b549eaa63fe7027c45f738ea3570e57c39
DharanyaSakthivel-SF4210 <Dharanya.sakthivel@syncfusion.com>
2025-01-29 06:18:51 +0530

    930350-Protect-Word-Documents

---
+++ Encrypt-Word-document-with-password/Encrypt-Word-document-with-password/Program.cs
@@ -0,0 +1,29 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System.Data;
+using System.IO;
+
+namespace Encrypt_Word_document_with_password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Open the file as a stream.
+            using (FileStream fileStream = new FileStream(Path.GetFullPath(@"Data/Template.docx"), FileMode.Open, FileAccess.ReadWrite))
+            {
+                //Load an existing Word document.
+                using (WordDocument document = new WordDocument(fileStream, FormatType.Docx))
+                {
+                    //Encrypt the Word document with a password.           
+                    document.EncryptDocument("password");
+                    // Save the Word document.
+                    using (FileStream outputStream = new FileStream(Path.GetFullPath(@"../../../Result.docx"), FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        document.Save(outputStream, FormatType.Docx);
+                    }
+                }
+            }
+        }
+    }
+}
